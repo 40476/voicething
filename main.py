5b1972cfcf2da6b0c8bd7a0fa4725fc295b48f75
usr_40476 <usr_40476@icloud.com>
2025-11-27 09:54:55 -0500

    Fix status tones not playing to sink device

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def log(msg):
 # =============== AUDIO ===============
 def play_tone(frequency=440, duration=0.05, sink="TTS_voice"):
     try:
-        cmd = f"play -n synth {duration} sine {frequency} vol 0.02"
+        cmd = f"sox -n -r 44100 -c 2 -b 16 -t raw - synth {duration} sine {frequency}"
         if sink:
             cmd += f" | pacat --client-name=TonePlayer --device={sink}"
         subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
